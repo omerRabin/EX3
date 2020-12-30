d7c28e9138a904e3208f81b7ba9addcd819a63a2
omerRabin <73130830+omerRabin@users.noreply.github.com>
2020-12-31 00:25:05 +0200

    start implementing GraphAlgo

--- src/GraphAlgo.py
+++ src/GraphAlgo.py
@@ -1,1 +1,15 @@
 from src import GraphAlgoInterface
+class GraphAlgo:
+def get_graph(self):
+
+def load_from_json(self, file_name: str):
+
+def save_to_json(self, file_name: str):
+
+def shortest_path(self, id1: int, id2: int):
+
+def connected_component(self, id1: int):
+
+def connected_components(self):
+
+def plot_graph(self):
