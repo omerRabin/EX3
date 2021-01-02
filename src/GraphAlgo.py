ac990048410520d5606a5152cc1412871e1e4dc9
Eldad122 <eldad.digorker@gmail.com>
2021-01-02 15:35:17 +0200

    some changes in GraphAlgo for now

--- src/GraphAlgo.py
+++ src/GraphAlgo.py
@@ -1,10 +1,16 @@
 from src import GraphAlgoInterface
+#from networkx.readwrite import json_graph
 class GraphAlgo:
 def get_graph(self):
 
 def load_from_json(self, file_name: str):
+    with open(file_name) as file:
+        js_graph = json.load(file)
+    return json_graph.node_link_graph(js_graph)
 
 def save_to_json(self, file_name: str):
+    #networkx.write_gml(self.graph, str)
+    #print json_graph.dumps()
 
 def shortest_path(self, id1: int, id2: int):
 
