cdfd7b5a7eee051843dbcda1768957d033e13d03
omerRabin <73130830+omerRabin@users.noreply.github.com>
2021-01-03 13:44:09 +0200

    start implementing GraphAlgo

--- src/Node.py
+++ src/Node.py
@@ -1,12 +1,17 @@
+from numpy.core import double
 from Edge_Data import Edge_Data
 
 
 class Node:
 
-    def __init__(self, node_id: int, pos: tuple, ni: dict[Node], info: str, tag: double, edges: dict[Edge_Data]):
+    def __init__(self, node_id: int, pos: tuple, ni: dict, info: str, tag: double, edges: dict[Edge_Data]):
         self.pos = pos
         self.node_id = node_id
         self.ni = ni
         self.info = info
         self.tag = tag
         self.edges = edges
+
+    def getNi(self):
+        return self.ni
+
