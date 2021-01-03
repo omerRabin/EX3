eb9c03a785d25bf3dfb89729c574b4eb80220859
omerRabin <73130830+omerRabin@users.noreply.github.com>
2021-01-04 00:28:37 +0200

    start implementing GraphAlgo

--- src/Edge_Data.py
+++ src/Edge_Data.py
@@ -1,10 +1,13 @@
+from numpy.core import double
 
 
 class Edge_Data:
-    def __init__(self, src: int, des: int, w: double, info: String, tag: int):
+    def __init__(self, src: int, des: int, w: double, info: str, tag: int):
         self.tag = tag
         self.info = info
         self.w = w
         self.des = des
         self.src = src
+
+
 
