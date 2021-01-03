5e2423c273bcfccc0b9cf35b2b6c77adf316f041
omerRabin <73130830+omerRabin@users.noreply.github.com>
2021-01-03 13:44:27 +0200

    start implementing GraphAlgo

--- src/DiGraph.py
+++ src/DiGraph.py
@@ -1,23 +1,25 @@
-def DiGraph(GraphInteface):
-    return None
 from src import GraphInterface
 from src import Node
 class DiGraph:
-    def __init__(self, graph: dict[Node], mc: int, sizeNodes: int, sizeEdges: int, size: int):
+    def __init__(self, graph: dict[Node], mc: int, sizeNodes: int, sizeEdges: int):
         self.graph=graph
         self.mc=mc
-        self.size=size
         self.sizeNodes=sizeNodes
         self.sizeEdges=sizeEdges
 
 
     def v_size(self):
         return self.sizeNodes
+
     def e_size(self):
         return self.sizeEdges
+
     def get_all_v(self):
         return self.graph
+
     def all_in_edges_of_node(self, id1: int):
+        n= self.get_node(self,id1)
+
 
     def all_out_edges_of_node(self, id1: int):
 
@@ -26,12 +28,12 @@ class DiGraph:
 
     def add_edge(self, id1: int, id2: int, weight: float):
 
-    def add_node(self, node_id: int, pos: tuple = None):
-
     def get_node(self, node_id: int):
         if node_id in self.graph:
             return self.graph.get(node_id)
-        pass
+            pass
+
+    def add_node(self, node_id: int, pos: tuple = None):
 
     def remove_node(self, node_id: int):
 
