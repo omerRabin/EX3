c0dd444461db20c3fd230b375a9e2383fdad90ac
omerRabin <73130830+omerRabin@users.noreply.github.com>
2021-01-04 15:01:55 +0200

    finish implementing DiGraph

--- src/Node.py
+++ src/Node.py
@@ -4,20 +4,16 @@ from Edge_Data import Edge_Data
 
 class Node:
 
-    def __init__(self, node_id: int, pos: tuple[double, double, double], ni: dict, info: str, tag: double,
+    def __init__(self, node_id: int, pos: tuple[double, double, double], info: str, tag: double,
                  edges: dict[int, Edge_Data]):
         self.pos = pos
         self.node_id = node_id
-        self.ni = ni
         self.info = info
         self.tag = tag
         self.edges = edges
-
-    def getNi(self):
-        return self.ni
 
     def getEdges(self):
         return self.edges
 
     def getWeight(self, id2):
-        return self.edges.get(id2).getWeight()
+        return self.edges.get(id2).w
