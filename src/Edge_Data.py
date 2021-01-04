7365b1b9c144444c38ac5ffb044bb73ba282e997
omerRabin <73130830+omerRabin@users.noreply.github.com>
2021-01-04 15:01:36 +0200

    finish implementing DiGraph

--- src/Edge_Data.py
+++ src/Edge_Data.py
@@ -2,7 +2,7 @@ from numpy.core import double
 
 
 class Edge_Data:
-    def __init__(self, src: int, des: int, w: double, info: str, tag: int):
+    def __init__(self, src: int, des: int, w: float, info: str, tag: int):
         self.tag = tag
         self.info = info
         self.w = w
