ec9a8d4aeca6a2aed95a1411fc8d48e99956d925
omerRabin <73130830+omerRabin@users.noreply.github.com>
2021-01-05 00:10:53 +0200

    some more tests

--- Tests/TestDiGraph.py
+++ Tests/TestDiGraph.py
@@ -27,6 +27,25 @@ class TestDiGraph(unittest.TestCase):
         g.add_edge(0, 1, 3)
         g.remove_edge(g.mc, 19)
 
+    def test_e_size(self):
+        g = init()
+        self.assertEqual(g.sizeEdges, 0)
+        g.add_edge(0, 1, 3.2)
+        self.assertEqual(g.sizeEdges, 1)
+        g.remove_edge(0, 1)
+        self.assertEqual(g.sizeEdges, 0)
+        g.add_edge(0, 1, 7)
+        g.add_edge(3, 1, 4.9)
+        g.remove_node(1)
+        self.assertEqual(g.sizeEdges, 0)
+
+    def test_get_all_v(self):
+        g = init()
+        for i in g.graph.keys():
+            self.assertEqual(True, i in g.graph.keys())
+
+    def test_all_in_edges_of_node(self):
+        
 
 if __name__ == '__main__':
     unittest.main()
