454bdcd97ff4f264019555d012637c2da7349e2e
omerRabin <73130830+omerRabin@users.noreply.github.com>
2021-01-11 21:51:42 +0200

    tets for save and load

--- Tests/TestGraphAlgo.py
+++ Tests/TestGraphAlgo.py
@@ -1,9 +1,23 @@
 import unittest
+from GraphAlgo import GraphAlgo
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+def init():
+    ga = GraphAlgo()
+    return ga
+
+
+class TestGraphAlgo(unittest.TestCase):
+
+    def test_loadFromJson(self):
+        ga = init()
+        self.assertEqual(ga.load_from_json("C:/Users/omer rabin/PycharmProjects/EX3/DATA/G_10_80_1.json"), True)
+        self.assertEqual(ga.load_from_json("blabla"), False)
+
+    def test_SaveToJson(self):
+        ga = init()
+        ga.load_from_json("C:/Users/omer rabin/PycharmProjects/EX3/DATA/G_10_80_1.json");
+        self.assertEqual(ga.save_to_json("save_test.json"), True)
 
 
 if __name__ == '__main__':
