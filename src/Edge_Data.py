1334d04629506dfd5901f4ea3a0c559137d252e5
omerRabin <73130830+omerRabin@users.noreply.github.com>
2021-01-11 21:49:57 +0200

    some additions to init definition

--- src/Edge_Data.py
+++ src/Edge_Data.py
@@ -1,12 +1,13 @@
 
 
 class Edge_Data:
-    def __init__(self, src: int, des: int, w: float, info: str, tag: int):
+    def __init__(self, src: int, des: int, w: float, info: str = None, tag: int = None):
         self.tag = tag
         self.info = info
         self.w = w
         self.des = des
         self.src = src
+
 
 
 
