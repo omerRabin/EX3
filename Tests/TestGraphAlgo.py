49573f5906e3159edc9ef8ccff34cf9d88ccf914
omerRabin <73130830+omerRabin@users.noreply.github.com>
2021-01-12 16:40:34 +0200

    write test to dfs

--- Tests/TestGraphAlgo.py
+++ Tests/TestGraphAlgo.py
@@ -40,9 +40,32 @@ class TestGraphAlgo(unittest.TestCase):
         self.assertEqual(g_algo.shortest_path(0, 1), (1, [0, 1]))
         self.assertEqual(g_algo.shortest_path(0, 2), (5, [0, 1, 2]))
 
+    def test_dfs(self):
+        ga = init()
+        for i in range(6):
+            ga.graph.add_node(i)
+        ga.graph.add_edge(1, 2, 4.2)
+        ga.graph.add_edge(2, 4, 5.2)
+        ga.graph.add_edge(2, 3, 0.9)
+        ga.graph.add_edge(3, 5, 9)
+        ga.graph.add_edge(4, 1, 0.8)
+        l = ga.dfs_help(2, ga.graph)
+        result = ga.graph.get_node(1) in l and ga.graph.get_node(2) in l and ga.graph.get_node(3) in l \
+                 and ga.graph.get_node(4) in l and ga.graph.get_node(5) in l
+        self.assertEqual(result, True)
+        ga.graph.remove_edge(1, 2)
+        l1 = ga.dfs_help(2, ga.graph)
+        result1 = ga.graph.get_node(1) in l1 and ga.graph.get_node(2) in l1 and ga.graph.get_node(3) in l1 \
+                 and ga.graph.get_node(4) in l1 and ga.graph.get_node(5) in l1
+        self.assertEqual(result1, True)
+        ga.graph.remove_edge(2, 3)
+        l2 = ga.dfs_help(2, ga.graph)
+        result2 = ga.graph.get_node(1) in l2 and ga.graph.get_node(2) in l2 and ga.graph.get_node(4) in l2
+        self.assertEqual(result2, True)
+
     def test_plot_graph(self):
         ga = init()
-        ga.load_from_json("C:/Users/omer rabin/PycharmProjects/EX3/DATA/G_10_80_1.json")
+        ga.load_from_json("C:/Users/omer rabin/PycharmProjects/EX3/DATA/A5")
         ga.plot_graph()
 
 
