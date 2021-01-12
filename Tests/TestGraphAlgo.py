b891b618fde8f13fbb7ddbfbbe78dac502a73cef
omerRabin <73130830+omerRabin@users.noreply.github.com>
2021-01-13 01:04:11 +0200

    finish tests

--- Tests/TestGraphAlgo.py
+++ Tests/TestGraphAlgo.py
@@ -30,15 +30,21 @@ class TestGraphAlgo(unittest.TestCase):
         ga.load_from_json("C:/Users/omer rabin/PycharmProjects/EX3/DATA/G_10_80_1.json")
         self.assertEqual(ga.save_to_json("save_test.json"), True)
 
-    def test_shortest_path(self):  # change code and more tests
+    def test_shortest_path(self):
         g_algo = GraphAlgo()
         g_algo.graph.add_node(0)
         g_algo.graph.add_node(1)
         g_algo.graph.add_node(2)
+        g_algo.graph.add_node(3)
         g_algo.graph.add_edge(0, 1, 1)
         g_algo.graph.add_edge(1, 2, 4)
         self.assertEqual(g_algo.shortest_path(0, 1), (1, [0, 1]))
         self.assertEqual(g_algo.shortest_path(0, 2), (5, [0, 1, 2]))
+        g_algo.graph.add_edge(0, 3, 8)
+        g_algo.graph.add_edge(2, 3, 2.5)
+        self.assertEqual(g_algo.shortest_path(0, 3), (7.5, [0, 1, 2, 3]))
+        g_algo.graph.add_edge(3, 1, 2.8)
+        self.assertEqual(g_algo.shortest_path(0, 1), (1, [0, 1]))
 
     def test_dfs(self):
         ga = init()
@@ -75,6 +81,10 @@ class TestGraphAlgo(unittest.TestCase):
         l = ga.connected_component(2)
         result = ga.graph.get_node(1) in l and ga.graph.get_node(2) in l and ga.graph.get_node(4) in l
         self.assertEqual(result, True)
+        ga1 = init()
+        self.assertEqual([], ga1.connected_component(90))
+        ga1.graph.add_node(0)
+        self.assertEqual([ga.graph.get_node(0)], ga.connected_component(0))
 
     def test_connected_components(self):  # not work- fix and then do test
         ga = init()
@@ -86,7 +96,9 @@ class TestGraphAlgo(unittest.TestCase):
         ga.graph.add_edge(3, 5, 9)
         ga.graph.add_edge(4, 1, 0.8)
         lists = ga.connected_components()
-        a = 5
+        result = [ga.graph.get_node(1), ga.graph.get_node(2), ga.graph.get_node(4)] in lists and \
+                 [ga.graph.get_node(0)] in lists and [ga.graph.get_node(3)] in lists and [ga.graph.get_node(5)] in lists
+        self.assertEqual(True, result)
 
     def test_plot_graph(self):
         ga = init()
