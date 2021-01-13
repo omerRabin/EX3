37c4d14f4efd5148549aeae50216ba068cc0cb43
Eldad122 <eldad.digorker@gmail.com>
2021-01-13 21:00:50 +0200

    starting part 3

---
+++ Tests/Part3.py
@@ -0,0 +1,17 @@
+import json
+import time
+import unittest
+
+import matplotlib.pyplot as plt
+
+from src.DiGraph import DiGraph
+from src.GraphAlgo import GraphAlgo
+import networkx as nx
+
+class MyTestCase(unittest.TestCase):
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
